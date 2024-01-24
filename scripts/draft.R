f7ebc9f7d4c7ddfe1f34ec54620c69dfb9c5f12c
Jose P <jospablo777@gmail.com>
2024-01-24 15:44:47 -0600

    Included time series analysis and tested if the stock diversity can increase sales

--- scripts/draft.R
+++ scripts/draft.R
@@ -5,6 +5,15 @@ library(reticulate)
 source('utils/get_data.R')
 source('utils/data_processing.R')
 
+# Time series libraries
+library(fable)
+library(tsibble)
+library(feasts)
+library(tsibbledata)
+library(fabletools)
+library(modeltime)
+library(timetk)  
+
 
 
 # -----------------------------------------------------------------
@@ -87,7 +96,7 @@ aa <- iowa_liquor_data %>%
 oldest_entry <- min(iowa_liquor_data$date)
 newest_entry <- max(iowa_liquor_data$date)
 
-time_range <- interval(oldest_entry, newest_entry)
+time_range <- lubridate::interval(oldest_entry, newest_entry)
 time_length(time_range, unit="years")
 # So we have 12 years of data, that's a lot!
 
@@ -193,23 +202,31 @@ ggplotly(anomalies_plot)
 # Saturday, and Sunday), this, since more of the businesses are closed on 
 # weekends, hence no purchases there, all good. But wait a second what are those in the 4th 
 # and the 11th of October 2013, those look suspicious. In fact, lets check all 
-# days in which more than 150k bottles were bought
+# all the anomalies over the percentile 50%. We are not interested in the lower anomalies 
+# because we already know that there are some purchases on weekends, but the
+# majority of establishments do not put orders on weekends
 
-days_more_than_15k_bottles <- iforest_results %>% 
-  filter(n_bottles > 150000)
+percentiles <- seq(from = 0, to = 1, by = 0.05)
+percentile_bottles <- quantile(iforest_results$n_bottles, percentiles)
 
-sum(days_more_than_15k_bottles$Anomaly == 1) / (sum(days_more_than_15k_bottles$Anomaly == 1) + sum(days_more_than_15k_bottles$Anomaly == 0))
+percentile_bottles
+
+anomalies_over_p50_bottles <- iforest_results %>% 
+  filter(n_bottles > percentile_bottles['50%'], 
+         Anomaly == 1)
+
+# sum(days_more_than_15k_bottles$Anomaly == 1) / (sum(days_more_than_15k_bottles$Anomaly == 1) + sum(days_more_than_15k_bottles$Anomaly == 0))
 # 55% are anomalies
 
 # Further analysis
-days_more_than_15k_bottles <- days_more_than_15k_bottles %>% 
+anomalies_over_p50_bottles <- anomalies_over_p50_bottles %>% 
   mutate(month_number     = month(day),
          month_day_number = mday(day),
          year_day_number  = yday(day))
 
 
-daynumber_by_month <- ggplot(days_more_than_15k_bottles, aes(x=month_day_number, y=month_number)) +
-  geom_point(aes(color=Anomaly)) +
+daynumber_by_month <- ggplot(anomalies_over_p50_bottles, aes(x=month_day_number, y=month_number)) +
+  geom_point() +
   xlab("")
 
 daynumber_by_month
@@ -224,8 +241,8 @@ daynumber_by_month_holidays <- holidays %>%
   summarise(mean_month = mean(month_number),
             mean_day   = mean(month_day_number))
 
-daynumber_by_month <- ggplot(days_more_than_15k_bottles, aes(x=month_day_number, y=month_number)) +
-  geom_point(aes(color=Anomaly)) +
+daynumber_by_month <- ggplot(anomalies_over_p50_bottles, aes(x=month_day_number, y=month_number)) +
+  geom_point() +
   geom_point(data=daynumber_by_month_holidays, aes(x=mean_day, y=mean_month), size=7, color='green', alpha=0.5) +
   geom_text(data = daynumber_by_month_holidays, nudge_x = 0.25, nudge_y = 0.25, 
             check_overlap = T, aes(label=Event, x=mean_day, y=mean_month)) +
@@ -234,9 +251,177 @@ daynumber_by_month <- ggplot(days_more_than_15k_bottles, aes(x=month_day_number,
 daynumber_by_month
 ggplotly(daynumber_by_month)
 
+# Jitter
+ggplot(anomalies_over_p50_bottles, aes(x=month_number, y=month_day_number)) +
+  geom_jitter() +
+  geom_point(data=daynumber_by_month_holidays, aes(x=mean_month, y=mean_day), size=7, color='green', alpha=0.5) +
+  geom_text(data = daynumber_by_month_holidays, nudge_x = 0.25, nudge_y = 0.25,
+            check_overlap = T, aes(label=Event, x=mean_month, y=mean_day)) +
+  xlab("")
 
-ggplot(days_more_than_15k_bottles, aes(x=year_day_number)) +
+
+ggplot(anomalies_over_p50_bottles, aes(x=year_day_number)) +
   geom_density(color="darkblue", fill="lightblue")
+# The more heavy drinking days of the year are concentrated at the end of the year
+
+
+# -----------------------------------------------------------------
+# Time series exploration
+
+consumption_day_summary %>% 
+  select(day, n_bottles) %>% 
+  mutate(day=ymd(day)) %>% 
+  as_tsibble(index=day) %>%
+  fill_gaps(n_bottles = 0) %>% # We will assume that for the days with no data are because not purchases occurred, which have sense since most of them are on weekends (i.e. lot of people don't work)
+  model(stl = STL(n_bottles)) %>% 
+  components() %>% 
+  autoplot()
+  
+# The above is a lot of information, let's aggregate the info in months 
+# to analize the chart with less granularity
+consumption_day_summary %>% 
+  select(day, n_bottles) %>% 
+  group_by(month=yearmonth(day)) %>%
+  summarise(n_bottles=sum(n_bottles)) %>% 
+  as_tsibble(index=month) %>%
+  model(stl = STL(n_bottles)) %>% 
+  components() %>% 
+  autoplot() %>% ggplotly()
+# Seasonality is interesting across the year we have three peaks, first in June (the more modest), the one in October, and another in December
+# We observe a trend increasing in alcohol "consumption" or at least in acquisition by the stores,
+# it is improvable that this is due to an increase in the population since it didn' increased that much (show chart).
+# Could it be due to an increase in the diversity of available products? Are the stores now able to supply a broader
+# spectrum of tastes? Let's see
+
+iowa_liquor_data %>% 
+  select(date, itemno) %>% 
+  group_by(month=yearmonth(date)) %>% 
+  summarise(n_different_products=n_distinct(itemno)) %>% 
+  as_tsibble(index=month) %>%
+  model(stl = STL(n_different_products)) %>% 
+  components() %>% 
+  autoplot() %>% 
+  ggplotly()
+# Ulalà... look at this beauty! Despite the obvious trend in more products entering 
+# to the market across time, we can observe that there is an spike in the diversity
+# of the available products. Could this be to the proximity to the end of the year 
+# festivities? Are the stores exposing the potential consumers to new products
+# that the can aquire at the end of the year?
+
+# But yeah the increase in alcoholic beverages could be due to more 
+# availability for all the tastes in the market. Maybe I don't like raspberry vodka
+# but I love tamarind Smirnoff, the rum with a funny pirate in the bottle, and so on...
+
+# Citation here: Hyndman and Athanasopoulos (2021). Forecasting: Principles and Practice. https://otexts.com/fpp3/
+
+
+# Lets check if the amount of different products available in the market
+# are a good predictor of units sold. Please notice that diversity no only comes
+# in flavors, it also comes with sizes and presentations. Let's say an smaller
+# bottle of the same rum, or an special tequila edition of your favorite brand
+# that includes collective shot glasses
+sale_by_diversity <- iowa_liquor_data %>% 
+  select(date, itemno, sale_bottles) %>% 
+  group_by(month = yearmonth(date)) %>%   # We will explore monthly data in our (available) history
+  summarise(n_different_products = n_distinct(itemno),
+            bottles_sale         = sum(sale_bottles))
+# Better not to use sale_dollars since the inflation will be a confounder. If we wanted to
+# include dollars, we could adjust for inflation and then remove the effect of this 
+# confounder
+
+
+ggplot(sale_by_diversity, aes(x=n_different_products, y=bottles_sale)) + 
+  geom_point()+
+  geom_smooth(method=lm, se=FALSE)
+# Very nice! It seems that the diversity of products is a good predicto. Now, 
+# we can get more numerical info from this if we run a linear model
+
+# Linear model
+fit_sales_by_diversity <- lm(bottles_sale ~ n_different_products, data=sale_by_diversity)
+summary(fit_sales_by_diversity)
+
+
+# There are a lot of good tutorials and textbook that dig deeper
+# into the validation of linear models, so we will skip that part and use
+# gvlma  to check the assumptions in a time saving manner. 
+
+gv_sales_by_diversity <- gvlma::gvlma(fit_sales_by_diversity)
+summary(gv_sales_by_diversity)
+# The only assumption don't met was the Link Function one. This suggest that the 
+# we misspecified the link function, or that we miss an important predictor in 
+# our model. You can check more in Peña and Slate (2006) Global Validation of Linear Model Assumptions.
+# Since the model fulfill the  Skewness, Kurtosis, and Heteroscedasticity assumptions,
+# we will put the link function issue to the side (for now).
+
+# Let's interpret our model
+summary(fit_sales_by_diversity)
+# First, we can observe in the R-squared that the model can explain 70% of our data
+# variability, i.e., the diversity in alcoholic products is a good predictor,
+# over all liquor sale in Iowa.
+# Second, we can observe that the estimate for the variable n_different_products is
+# 890. We interpret this as follows: with the release of each new product in the market,
+# the monthly (alcohol) sales in Iowa will increase in 890 bottles in the State of Iowa
+
+# But how to present this information to non-technical people?
+
+# Let's say that youre working for a liquor distributor, and now you're 
+# presenting your findings to the C-level execs. You can say something like "For 
+# every new product (or new presentation of our old product) we launch in Iowa 
+# we will sell around 900 units monthly" # Notice that we say "around 900" to facilitate the diggestion of the info. Round numbers decrease the cognitive load, are easy to remember, and will be easier to asssimilate them by your audience.
+# Of course it not be that simple, if the product is well liked by the population it will sell
+# even more, but if it sucks it wont sell. Also, having more products in the inventory could
+# increase costs in logistics, marketing, and so on.
+
+
+# Presenting this to the gov and health entities. Now let's assume that you're a consultant
+# for a local health organization trying to fight the aftermath of excessive drinking. You would
+# present the results such as "The increase in diversity makes the alcohol consumption more appealing
+# to the consumer, so we can propose an extra tax to the sellers above X amount of different alcoholic products in the inventory. 
+# The amount collected will be directed to fund rehabilitation clinics and..."
+
+
+# Remember that link function assumption violation? Let's go back to that.
+# So, we didn't accomplish all the assumptions proposed by Peña and Slate (2006),
+# their approach is  quite interesting since the proposed framework includes a 
+# global metric, and consider the interaction between assumptions.
+
+
+# So, lets back to our first model. We will do the same, just with a little difference. 
+# Our variables will be log transformed
+ggplot(sale_by_diversity, aes(x=log(n_different_products), y=log(bottles_sale))) + 
+  geom_point() +
+  geom_smooth(method=lm, se=FALSE)
+# The plot does not seem that different
+
+# Linear model, log transformed variables
+fit_sales_by_diversity_log <- lm(log(bottles_sale) ~ log(n_different_products), data=sale_by_diversity)
+# Check assumptions
+gv_sales_by_diversity_log <- gvlma::gvlma(fit_sales_by_diversity_log)
+summary(gv_sales_by_diversity_log)
+# Great! Now we fulfill our global stat, so we can say that our inference from
+# this model will be more robust
+
+# Now let's dissect our model
+summary(fit_sales_by_diversity_log)
+
+# Still, the model can explain 70% of the variability in the data, but now
+# the coefficients are different. One of the advantages of the Log-Log regression
+# (log dependent variable, log predictor) is its intuitive interpretation: for
+# every increase in 1% of X we will have a β% increase (or decrease if β is negative) 
+# in our dependent variable. In this case the estimate for β is 1.02, that mean
+# that if we increase our diversity of products by 1% we will sell 100% more bottles,
+# in other words, we duplicate (x2) the amount of bottles sold every time we 
+# diversify in 1% our stock. That's a lot, but again we have to considerate also
+# the cost in logistics, space available in our shelf and cellars, 
+
+
+
+# -----------------------------------------------------------------
+# Some forecasting for 2024
+# This will be good to know in which days will be better to replenish the stores
+# check
+# https://www.business-science.io/code-tools/2020/06/29/introducing-modeltime.html?utm_content=buffer7ae04&utm_medium=social&utm_source=linkedin.com&utm_campaign=buffer
+# QUEDÉ ACÁ
 
 
 # -----------------------------------------------------------------
