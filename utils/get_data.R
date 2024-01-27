0e658ecc539728b5b9a8cbfee571779762ffaf69
Jose P <jospablo777@gmail.com>
2024-01-26 19:24:18 -0600

    Documented functions.

--- utils/get_data.R
+++ utils/get_data.R
@@ -53,8 +53,25 @@ download_liquor_data <- function(total_of_rows=28176372, batch_size=1000000) {
 }
 
 # ----------------------------------------------------------------
-# Generalform to download data in batches
-download_iowa_data <- function(data_id, data_name, total_of_rows=28176372, batch_size=1000000) {
+# General form to download data in batches
+
+#' Data set batch downloader
+#' 
+#' `download_iowa_data` downloads specified data from the Iowa Open Data portal (https://data.iowa.gov/).
+#'
+#' @param data_id A string. You get it from the data set page in. This can be found on the url of your data set of interest; for example the url for Iowa Fire Department Census data set is "https://data.iowa.gov/Emergency-Management/Iowa-Fire-Department-Census/hv43-6ksq/about_data" here the id is the value nex to the data set name on the right (i.e., "hv43-6ksq").
+#' @param folder A string. The path to the folder you will save the data.
+#' @param data_name A string. How your files will be called in your file system.
+#' @param total_of_rows An integer. The size of the dataset, or the amount of rows you want to pull from the API.
+#' @param batch_size An integer. The size of the batch (in rows) you want to pull, the default is 1M rows.
+#'
+#' @return Nothing. The function save the batches in .csv files in the folder you've chosen for 
+#' @export 
+#'
+#' @examples
+#' # Here we download the Iowa Fire Department Census data set. If you go to the dataset web page (https://data.iowa.gov/Emergency-Management/Iowa-Fire-Department-Census/hv43-6ksq/about_data), you will find that the total number of rows of this data set (to the date, 26-Jan-2024) is 738, so if we want the whole data set it is okay if we set total_of_rows=1000, and batch_size=1000.
+#' download_iowa_data(data_id='hv43-6ksq', folder='data', data_name='fire_department_census', total_of_rows=1000, batch_size=1000)
+download_iowa_data <- function(data_id, folder, data_name, total_of_rows=10000, batch_size=5000) {
   # to the date, we have a total of 28176372 rows
   
   batch_size_str <- format(batch_size, scientific = F)
@@ -84,7 +101,7 @@ download_iowa_data <- function(data_id, data_name, total_of_rows=28176372, batch
     df <- httr::content(request)
     
     # we will save our data locally to save time
-    file_name <- paste0('data/', data_name, i, '.csv')
+    file_name <- paste0(folder, '/', data_name, '_', i, '.csv')
     write_csv(df,file_name)
     
     offset <- offset + batch_size
@@ -125,9 +142,20 @@ read_liquor_data <- function(folder_path='data') {
 
 # ----------------------------------------------------------------
 # General form to read all the downloaded files into a single df
+#' Title
+#'
+#' @param folder_path A string. The path to the folder you saved your data.
+#' @param data_name A string. How your data set common files are called in your file system.
+#'
+#' @return A data frame. It reads into the memory the chunks of your data set downloaded in batches from the Iowa Open Data portal (https://data.iowa.gov/).
+#' @export
+#'
+#' @examples
+#' # Read the fire_department_census data you downloaded with th download_iowa_data() function. The folder 'data' with the data is located in your R project working directory.
+#' read_iowa_data(folder_path='data', data_name='fire_department_census')
 read_iowa_data <- function(folder_path='data', data_name) {
   filenames <- list.files(folder_path, 
-                          pattern=paste0(data_name, ".*\\.csv$"), # Regex to match data_nameXXX.csv
+                          pattern=paste0(data_name, ".*\\.csv$"), # Regex to match data_name_XXX.csv
                           full.names=TRUE)
   
   for(i in 1:length(filenames)){
